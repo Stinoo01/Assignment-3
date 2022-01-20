783349f92d4197fcc867f8e2c320228b096e6023
Stinoo01 <93342130+Stinoo01@users.noreply.github.com>
2022-01-20 11:53:14 +0100

    Update revp.py

--- Assignment3/revp.py
+++ Assignment3/revp.py
@@ -1,27 +1,28 @@
-import string
-
-
-def writing(dna):
+#function
+def count(dna):
 	DNA=""
-	for i in range(len(dna)):
-		if(dna[i]=='A'):
+	for x in range(len(dna)):
+		if(dna[x]=='A'):
 			DNA+='T'
-		elif(dna[i]=='T'):
+		elif(dna[x]=='T'):
 			DNA+='A'
-		elif(dna[i]=='C'):
+		elif(dna[x]=='C'):
 			DNA+='G'
-		elif(dna[i]=='G'):
+		elif(dna[x]=='G'):
 			DNA+='C'
 	return DNA
 
 def reverse(dnas):
-	return dnas[len(dnas)::-1]
-#first input is the fasta format: skip 
-fasta=input()
-#second input is the file. make sure tht the file has no spaces.
+	return dnas[len(dnas)::-1] #reverse the string 
+
+#i have to write on the consol the imput from rosalind
+#first input is the fasta format
+fasta=input() #skip
+#second input is the file. make sure that the file has no spaces.
 DNA_string=input()
-for i in range(len(DNA_string)):
-	for j in range(4,14):
-		if (DNA_string[i:i+j]==reverse(writing(DNA_string[i:i+j]))and i+j<=len(DNA_string)):
-			print(i+1, end=" ")
-			print(j)
+for k in range(len(DNA_string)):
+	#palindrome sequence between 4 and 12
+	for j in range(4,12):
+		if (DNA_string[k:k+j]==reverse(count(DNA_string[k:k+j]))and k+j<=len(DNA_string)):
+			#output
+			print(k+1, j)
